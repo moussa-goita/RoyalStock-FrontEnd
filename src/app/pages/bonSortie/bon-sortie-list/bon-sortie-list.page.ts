12eef85307345980877b8e7b07bbd3a8bdbb67c4
moussa-goita <mmgoita19@gmail.com>
2024-08-13 15:47:21 +0000

    Bonsortie liste Ok

--- src/app/pages/bonSortie/bon-sortie-list/bon-sortie-list.page.ts
+++ src/app/pages/bonSortie/bon-sortie-list/bon-sortie-list.page.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { addIcons } from 'ionicons';
@@ -28,6 +28,9 @@ import {
   IonTitle,
   IonToolbar
 } from '@ionic/angular/standalone';
+import { BonSortie } from 'src/app/models/bon-sortie';
+import { AuthService } from 'src/app/services/auth.service';
+import { BonSortieService } from 'src/app/services/bon-sortie.service';
 
 @Component({
   selector: 'app-bon-sortie-list',
@@ -62,12 +65,63 @@ import {
     IonSearchbar,
   ],
 })
-export class BonSortieListPage  {
-  constructor(private router: Router) {
+export class BonSortieListPage implements OnInit  {
+  bonSorties: BonSortie[] = [];
+  filteredBonSorties: BonSortie[] = [];
+  bonSortieToDelete: number | null = null;
+  selectedBonSortie: BonSortie | null = null;
+  infoMessage= '';
+  errorMessage= '';
+
+
+  constructor(private router: Router, private authService: AuthService, private BonSortieService: BonSortieService) {
     addIcons({
       calendar,print, document, clipboard, person, create, add, eye, trash
     
     });
+  }
+  ngOnInit(): void {
+    this.loadBonSorties();
+  }
+  loadBonSorties(): void {
+    const currentUser = this.authService.currentUserValue;
+    if (currentUser && currentUser.entrepot) {
+      const entrepotId = currentUser.entrepot.entrepotId;
+      this.BonSortieService.getBonSortiesByEntrepots(entrepotId).subscribe(data => {
+        console.log('Données reçues:', data); // Debugging
+        if (data.length === 0) {
+          this.infoMessage = 'Aucun Bon Entrees trouvée pour cet Entrepot.';
+          setTimeout(() => this.infoMessage = '', 2000);
+        } else {
+          this.bonSorties = data;
+          this.filteredBonSorties = data;
+        }
+      }, error => {
+        console.error('Erreur lors de la récupération des Bon Entrees:', error);
+        this.errorMessage = 'Erreur lors de la récupération des Bon Entrees.';
+      });
+    } else {
+      this.errorMessage = 'Erreur: entrepôt utilisateur non trouvé';
+    }
+  }
+  
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.filteredBonSorties = this.bonSorties.filter(bonSortie =>
+      bonSortie.motif && bonSortie.motif.title && bonSortie.motif.title.toLowerCase().includes(filterValue)
+    );
+  }
+
+  deleteBonSortie(id: number): void {
+    this.BonSortieService.deleteBonSortie(id).subscribe(() => {
+      this.bonSorties = this.bonSorties.filter(b => b.id !== id);
+      this.filteredBonSorties = this.filteredBonSorties.filter(b => b.id !== id);
+    });
+  }
+
+  printBonSortie(id: number): void {
+    this.router.navigate(['/print-bon-sortie', id]);
   }
 
   goToAddForm() {
@@ -78,5 +132,8 @@ export class BonSortieListPage  {
   }
 goToAddDetail(){
   this.router.navigate(['/bon-sortie-detail']);
+}
+hasRole(role: string): boolean {
+  return this.authService.hasRole(role);
 }
 }

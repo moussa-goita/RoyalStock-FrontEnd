4727c806dbaf09a3440bd0c01146fad178e64d33
reyptz <reypotozy@gmail.com>
2024-08-15 17:15:22 +0000

    Moi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,36 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { IonApp, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuButton, IonMenuToggle, IonNote, IonRouterLink, IonRouterOutlet, IonSplitPane, IonTitle, IonToggle, IonToolbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { archiveOutline, archiveSharp, bookmarkOutline, bookmarkSharp, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import {
+  homeOutline,
+  homeSharp,
+  cubeOutline,
+  cubeSharp,
+  enterOutline,
+  enterSharp,
+  exitOutline,
+  exitSharp,
+  pricetagsOutline,
+  pricetagsSharp,
+  keyOutline,
+  keySharp,
+  businessOutline,
+  businessSharp,
+  peopleOutline,
+  peopleSharp,
+  personOutline,
+  personSharp,
+  compassOutline,
+  notificationsOutline,
+  receiptOutline,
+  statsChartOutline,
+  bookmarksOutline,
+  compassSharp,
+  notificationsSharp,
+  receiptSharp,
+  statsChartSharp,
+  bookmarksSharp, invertModeOutline, invertModeSharp
+} from 'ionicons/icons';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -15,33 +44,50 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
   public appPages: Array<{ title: string, url: string, icon: string, role: string[] }> = [];
+  public currentUserRole: string | any;
 
   constructor(public authService: AuthService, private router: Router) {
-    this.initializeAppPages();
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
-   
+    this.authService.currentUser.subscribe(user => {
+      this.currentUserRole = user?.role;
+      this.initializeAppPages();
+    });
 
+    addIcons({
+      homeOutline, homeSharp,
+      cubeOutline, cubeSharp,
+      enterOutline, enterSharp,
+      exitOutline, exitSharp,
+      pricetagsOutline, pricetagsSharp,
+      keyOutline, keySharp,
+      businessOutline, businessSharp,
+      peopleOutline, peopleSharp,
+      personOutline, personSharp,
+      statsChartOutline, receiptOutline, bookmarksOutline,
+      compassOutline, notificationsOutline, compassSharp,
+      notificationsSharp, receiptSharp, statsChartSharp,
+      bookmarksSharp, invertModeOutline, invertModeSharp
+    });
   }
-  initializeAppPages() {
-    const currentUser = this.authService.currentUserValue;
 
-    if (!currentUser) {
+  initializeAppPages() {
+    if (!this.currentUserRole) {
       this.router.navigate(['/login']);
       return;
     }
 
-    const role = currentUser.role;
-
     this.appPages = [
-    { title: 'Dashboard', url: '/dashboard', icon: 'home', role: ['ADMIN', 'MANAGER']  },
-    { title: 'Bon Entrée', url: '/bon-entre-list', icon: 'enter', role: ['MANAGER'] },
-    { title: 'Bon Sortie', url: '/bon-sortie-list', icon: 'exit', role: ['VENDEUR', 'ADMIN', 'MANAGER'] },
-    { title: 'Fournisseurs', url: '/fournisseurs-list', icon: 'people', role: ['VENDEUR', 'MANAGER'] },
-    { title: 'Fournisseurs-Espace', url: '/fournisseurs-espace', icon: 'bookmarks', role: ['VENDEUR', 'MANAGER']},
-    { title: 'Notification', url: '/notifications', icon: 'notifications', role: ['ADMIN', 'MANAGER', 'USER'] }
-  ].filter(page => page.role.includes(role));
-}
-
+      {title: 'Dashboard', url: '/dashboard', icon: 'home', role: ['ADMIN', 'MANAGER', 'VENDEUR']},
+      {title: 'Bon Entrée', url: '/bon-entre-list', icon: 'enter', role: ['MANAGER']},
+      {title: 'Bon Sortie', url: '/bon-sortie-list', icon: 'exit', role: ['VENDEUR', 'MANAGER']},
+      {title: 'Fournisseurs', url: '/fournisseurs-list', icon: 'people', role: ['VENDEUR', 'MANAGER']},
+      {title: 'Fournisseurs-Espace', url: '/fournisseurs-espace', icon: 'bookmarks', role: ['VENDEUR', 'MANAGER']},
+      {title: 'Produits', url: '/produit', icon: 'cube', role: ['VENDEUR', 'MANAGER']},
+      {title: 'Entrepôt', url: '/entrepot', icon: 'compass', role: ['ADMIN']},
+      {title: 'Notification', url: '/notifications-list', icon: 'notifications', role: ['ADMIN', 'MANAGER', 'USER']},
+      {title: 'Rapport', url: '/rapport', icon: 'receipt', role: ['MANAGER']},
+      {title: 'Mon Profil', url: '/user-profile', icon: 'person', role: ['ADMIN', 'MANAGER', 'VENDEUR']},
+    ].filter(page => page.role.includes(this.currentUserRole));
+  }
 
   toggleDarkMode(event: any) {
     document.body.classList.toggle('dark', event.detail.checked);
--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -1,19 +1,80 @@
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonContent, IonHeader, IonList, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem, IonLabel,
+  IonList,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { UtilisateurService } from "../../services/utilisateur.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.page.html',
   styleUrls: ['./user-profile.page.scss'],
   standalone: true,
-  imports: [IonList, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonInput, IonButton, IonList, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonLabel]
 })
-export class UserProfilePage  {
+export class UserProfilePage implements OnInit {
 
-  constructor() { }
+  utilisateur: any = {}; // Define the utilisateur object
+  newPassword: string = '';
+  confirmPassword: string = '';
+  errorMessage: string = '';
 
-  
+  constructor(private UtilisateurService: UtilisateurService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile(): void {
+    const currentUser = this.authService.currentUserValue;
+    if (currentUser && currentUser.id) {
+      this.UtilisateurService.getUtilisateurById(currentUser.id).subscribe(
+        (data: any) => {
+          this.utilisateur = data;
+        },
+        (error) => {
+          console.error('Error fetching user profile', error);
+        }
+      );
+    }
+  }
+
+  updateProfile(): void {
+    if (this.newPassword && this.newPassword !== this.confirmPassword) {
+      this.errorMessage = 'Les mots de passe ne correspondent pas.';
+      return;
+    }
+
+    const updateData = {
+      ...this.utilisateur,
+      password: this.newPassword ? this.newPassword : undefined
+    };
+
+    this.UtilisateurService.updateUtilisateur(this.utilisateur.id, this.utilisateur).subscribe(
+      (data) => {
+        console.log('User profile updated successfully', data);
+      },
+      (error) => {
+        console.error('Error updating user profile', error);
+      }
+    );
+  }
+
+  get currentUser() {
+    return this.authService.currentUserValue;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 
 }

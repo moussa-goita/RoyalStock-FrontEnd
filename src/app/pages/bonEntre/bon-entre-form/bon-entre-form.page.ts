6834754a6979b7d79a1550c77ac0df35aa1dfd1c
balla1199 <ballacoulibaly187@gmail.com>
2024-08-19 11:01:23 +0100

    Mise à jour des fichiers bon d'entrée et bon de sortie

--- src/app/pages/bonEntre/bon-entre-form/bon-entre-form.page.ts
+++ src/app/pages/bonEntre/bon-entre-form/bon-entre-form.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bon-entre-form',
--- src/app/pages/bonEntre/bon-entre-list/bon-entre-list.page.ts
+++ src/app/pages/bonEntre/bon-entre-list/bon-entre-list.page.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({

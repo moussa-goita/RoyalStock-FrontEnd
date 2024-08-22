2badf8aa234c29df161b027c68216cf563292edc
Badra Aliou SY <sybadraaliou@gmail.com>
2024-08-22 15:46:29 +0000

    environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://192.168.78.35:8443/api'
+  apiUrl: 'http://localhost:8080/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://192.168.123.35:8443/api'
+  apiUrl: 'http://localhost:8080/api'
 };
 
 /*
